5dd94b36f319bb5cae181d5b901ef317653deaaf
DanteGut <dante.gutierrez@ivitec.mx>
2023-05-15 20:25:05 -0600

    v0.53.0 integracion boton Whatsapp

--- principal.php
+++ principal.php
@@ -60,6 +60,17 @@ use config\views;
         </div>
         <div class="menudv navbar col-md-4"> <a href="https://escuela.dveducacion.com/" target="_blank">Plataforma</a></div>
     </div>
+
+    <div id="whatsapp">
+        <a href="https://wa.me/3324339427" target="_blank" class="btn-whatsapp">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-whatsapp" width="55" height="55" viewBox="0 0 24 24" stroke-width="1.5" stroke="#003366" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+                <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" />
+            </svg>
+        </a>
+
+    </div>
 </section>
 
 <?php  include($data->include_action); ?>
